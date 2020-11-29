3666d7b414936d0ca2fbf72314cbf9c47f46f097
user <3gotosnapp@gmail.com>
2020-11-29 23:20:03 +0300

    Добавьте файлы проекта.

---
+++ Kinulo/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Kinulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1099, 623);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1124, 574);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AX";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1124, 610);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1124, 647);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Не помню зачем она нужна, я потерял тз";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1124, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1123, 48);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1124, 81);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1264, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "H0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1264, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "V0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1264, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "a";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1208, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(47, 22);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "50";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1124, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "1 метр это";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1262, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "px";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1124, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Сетка, где ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1124, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "клетка это ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1208, 162);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(48, 22);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.Text = "0";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1262, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "метр";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1421, 746);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Kinulo/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;//using System.Drawing
+using System.Windows.Forms;
+ 
+namespace Kinulo
+{
+    
+    public partial class Form1 : Form
+    {
+        double _vx, _vy, _v0, _agile, _h0, _dev = 50; int _setka = 0;
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _v0 = Convert.ToDouble(textBox2.Text)*_dev;
+            }
+            catch { 
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _setka = Convert.ToInt32(textBox5.Text) * Convert.ToInt32(_dev);
+            }
+            catch{
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _agile = Convert.ToDouble(textBox3.Text);
+            }
+            catch { }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _dev = Convert.ToDouble(textBox4.Text);
+            }
+            catch { }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _h0 = Convert.ToDouble(textBox1.Text)*_dev;
+            }
+            catch { }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawLine(new Pen(Brushes.Black, 1), new Point(0, 0), new Point(0, 500));
+            g.DrawLine(new Pen(Brushes.Black, 1), new Point(0, 500), new Point(900, 500));
+
+            if (_setka !=0) {
+
+                for (int i = _setka; i < 500; i = i + _setka) {
+                    g.DrawLine(new Pen(Brushes.Gray, 1), new Point(0, 500-i), new Point(900, 500-i));
+                   
+                }
+                for (int i = _setka; i < 900; i = i + _setka)
+                {
+                   
+                    g.DrawLine(new Pen(Brushes.Gray, 1), new Point(i, 0), new Point(i, 500));
+                }
+
+            }
+
+
+            // Create pen.
+        //    Pen blackPen = new Pen(Color.Black, 2);
+
+            // Create rectangle for ellipse.
+         //   Rectangle rect = new Rectangle(100, 100, 1, 1);
+
+            // Draw ellipse to screen.
+         //   g.DrawRectangle(blackPen, rect);
+/*
+            for (int i = 0; i < 50; i++) {
+                Pen blackPen1 = new Pen(Color.Black, 2);
+
+                Rectangle rect1 = new Rectangle(100, i, 1, 1);
+
+                
+                g.DrawRectangle(blackPen1, rect1);
+
+            }
+*/
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_agile == 0 || _v0 == 0)
+            {
+                textBox2.BackColor = Color.Red;
+                textBox3.BackColor = Color.Red;
+            }
+            else
+            {
+
+                double rad = _agile * (Math.PI / 180.0);
+
+                _vy = (Math.Sin(rad)) * _v0;
+                _vx = (Math.Cos(rad)) * _v0;
+                int vy = Convert.ToInt32(_vy);
+                int vx = Convert.ToInt32(_vx);
+                //button1.Text = Convert.ToString(_vy);
+                //  button2.Text = Convert.ToString(vy);
+                Graphics g = pictureBox1.CreateGraphics();
+                Pen blackPen1 = new Pen(Color.Black, 2);
+                double x = 0, y = 0;
+                int i = 0;
+                while (0 == 0)
+                {
+                    Rectangle rect1 = new Rectangle(Convert.ToInt32(x), Convert.ToInt32(500 - y), 1, 1);
+                    Console.Write(x);
+                    x = _vx * i;
+                    y = (_vy * i) - ((10 * i * i) / 2) + _h0;
+
+                    if (x > 900)
+                    {  /// было бы неплохо пофиксить, ибо тут должен отключаться цикл если значение переходят в отрицательный у
+                        break;
+                    }
+
+                    try
+                    {
+                        g.DrawRectangle(blackPen1, rect1);
+                    }
+                    catch { }
+                    i++;
+                }
+            }
+        }
+    }
+}
